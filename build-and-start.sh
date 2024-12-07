ba5aa0b5799efa914496376df8726c97054ccbfc
Giacomo Romano <giacomixx.96@gmail.com>
2024-12-07 23:27:44 +0100

    Upgrade configuration postres and docker for exec inside container

---
+++ build-and-start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$1" != "local" && "$1" != "prod" ]]; then
+  echo "Warning: Invalid input. Please provide either 'local' or 'prod' for the relevant environment."
+  exit 1
+fi
+
+ENV="$1"
+
+# Sets the variable for the reference environment
+export ENV
+
+[ ! -z "$SKIP_BUILD" ] || ./mvnw clean package -DskipTests
+
+docker build \
+  -t houseofpizza:latest \
+  --build-arg SOURCE_JAR=./target \
+  .
+
+docker compose up --build
+
+# See http://localhost:4001/houseofpizza/swagger-ui/index.html
